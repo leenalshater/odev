6202ea4125193f512c179de40be295bf6a0fd8c6
leenalshater <101742318+leenalshater@users.noreply.github.com>
2022-03-17 21:53:17 +0300

    Add files via upload

---
+++ Models/odevim.cs
@@ -0,0 +1,7 @@
+public class uye{
+    public string ad{get;set;}
+    public string soyad{get;set;}
+    public string email{get;set;}
+    public DateTime dogumTarihi{get;set;}
+    public double puanOrtalamasi{get;set;}
+ }
